581802dbc89c0712e4569e5cc12e7e9654bb6938
Willyan Geraldo <willyangeraldo@Willyans-MacBook-Pro.local>
2020-01-25 11:26:56 -0800

    styling cells

---
+++ src/components/App/index.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import './App.scss';
+import NumberDisplay from '../NumberDisplay';
+import Button from '../Button';
+import { generateCells } from '../../utils';
+
+const App: React.FC = () => {
+    const [cells, setCells] = useState(generateCells());
+
+    const renderCells = (): React.ReactNode => {
+        return cells.map((row, rowIndex) =>
+            row.map((cell, colIndex) => (
+                <Button key={`${rowIndex}-${colIndex}`} />
+            )),
+        );
+    };
+    return (
+        <div className="App">
+            <div className="Header">
+                <NumberDisplay value={0} />
+                <div role="button" className="Face">
+                    <span role="img" aria-label="face">
+                        😄
+                    </span>
+                </div>
+                <NumberDisplay value={23} />
+            </div>
+            <div className="Body">{renderCells()}</div>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Button.scss';
+
+const Button = () => {
+    return <button className="Button">😄</button>;
+};
+
+export default Button;
