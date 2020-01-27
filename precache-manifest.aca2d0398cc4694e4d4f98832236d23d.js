d75852bd1458a2ec5ee4f391f1a2ffb8d2b08360
Willyan Geraldo <willyangeraldo@Willyans-MacBook-Pro.local>
2020-01-26 19:04:31 -0800

    Updates

---
+++ precache-manifest.aca2d0398cc4694e4d4f98832236d23d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb4c0f1efd5d9d5b83519dc1d84f3d66",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "5b598a12468fb40f3da7",
+    "url": "/minesweeper/static/css/main.bdfaeae8.chunk.css"
+  },
+  {
+    "revision": "b8318eef7fd14d42618d",
+    "url": "/minesweeper/static/js/2.939e20c5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/minesweeper/static/js/2.939e20c5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b598a12468fb40f3da7",
+    "url": "/minesweeper/static/js/main.0bae6e49.chunk.js"
+  },
+  {
+    "revision": "81fac3eb777183ec555d",
+    "url": "/minesweeper/static/js/runtime-main.70dc93c3.js"
+  }
+]);
